90700f6854823521228672cdaab0b79af3c0218b
Sathappan89 <sathappanas1989@gmail.com>
2025-11-09 23:14:56 -0700

    D6_Java_SwichCaseConcept

---
+++ src/javasessions/SwichCaseConcept.java
@@ -0,0 +1,29 @@
+package javasessions;
+
+public class SwichCaseConcept {
+
+	public static void main(String[] args) {
+
+		String browser = "Firefox ";
+
+		switch (browser.toLowerCase().trim()) {
+		case "chrome":
+			System.out.println("launch chrome");
+			break;
+		case "firefox":
+			System.out.println("launch firefox");
+			break;
+		case "edge":
+			System.out.println("launch edge");
+			break;
+		case "safari":
+			System.out.println("launch safari");
+			break;
+		default:
+			System.out.println("invalid browser...please pass the right browser name..");
+			break;
+		}
+
+	}
+
+}
