f64e112b2d4c31e8f6d298f80acb860664abd819
Sathappan89 <sathappanas1989@gmail.com>
2025-11-10 22:53:12 -0700

    D7_Java_MaxNumber

--- src/javasessions/MaxNumber.java
+++ src/javasessions/MaxNumber.java
@@ -3,23 +3,21 @@ package javasessions;
 public class MaxNumber {
 
 	public static void main(String[] args) {
-		
+
 		int x = 700;
 		int y = 500;
 		int z = 800;
-		
+
 		// && - Short circuit operator - Improve the performance
 		// &
-		if(x>y && x>z) { //F && No
+		if (x > y && x > z) { // F && No
 			System.out.println("x is the greatest number");
-		}
-		else if(y>z) { //F
+		} else if (y > z) { // F
 			System.out.println("y is the greatest");
-		}
-		else {
+		} else {
 			System.out.println("z is the greatest");
 		}
-		
+
 //		if(10>5 && 10>7 && 10<20 && 10>30) {
 //			System.out.println("Hi");
 //		}
@@ -27,5 +25,4 @@ public class MaxNumber {
 //			System.out.println("bye");
 //		}
 	}
-
 }
