68cc3dcfdc3daecc480480be4b03c3ed6caf6d63
Sathappan89 <sathappanas1989@gmail.com>
2025-11-11 21:54:42 -0700

    Maximum4Numbers

--- src/javasessions/MaximumNumber.java
+++ src/javasessions/MaximumNumber.java
@@ -3,22 +3,19 @@ package javasessions;
 public class MaximumNumber {
 
 	public static void main(String[] args) {
-		
+
 		int a = 900;
 		int b = 200;
 		int c = 1300;
 		int d = 400;
-		
-		if(a>b && a>c && a>d) {
+
+		if (a > b && a > c && a > d) {
 			System.out.println(a + " is the MaxNumber");
-		}
-		else if(b>c && b>d) {
+		} else if (b > c && b > d) {
 			System.out.println(b + " is the MaxNumber");
-		}
-		else if(c>d) {
+		} else if (c > d) {
 			System.out.println(c + " is the MaxNumber");
-		}
-		else {
+		} else {
 			System.out.println(d + " is the MaxNumber");
 		}
 
