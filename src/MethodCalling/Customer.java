40066a669ec23d1f95e0645f724d60e2d8662e89
Sathappan89 <sathappanas1989@gmail.com>
2025-11-24 21:12:37 -0700

    Call By Reference - Another way

--- src/MethodCalling/Customer.java
+++ src/MethodCalling/Customer.java
@@ -2,26 +2,26 @@ package MethodCalling;
 
 public class Customer {
 
+	static Customer obj;
+
 	public void m1() {
 		System.out.println("m1 method");
+		Customer.t1();
 	}
-	
-	public static void t1(Customer ob) { //class parameter
+
+	public static void t1() { // class parameter
 		System.out.println("t1 method");
-		ob.m1();
-//		Customer c = new Customer();
-//		c.m1();
-	}
-	
-	public static void main(String[] args) {
-		
-		Customer obj=new Customer();
 		obj.m1();
-		
-		
-		Customer.t1(obj); //obj - reference
-		                  // call by reference
-		
+	}
+
+	public static void main(String[] args) {
+
+		obj = new Customer();
+		obj.m1();
+
+		Customer.t1(); // obj - reference
+						// call by reference
+
 	}
 
 }
