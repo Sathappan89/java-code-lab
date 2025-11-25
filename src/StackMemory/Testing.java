73801f9aac80b0998620d1a46af0f6104945c1ab
Sathappan89 <sathappanas1989@gmail.com>
2025-11-24 18:21:51 -0700

    StackMemory

---
+++ src/StackMemory/Testing.java
@@ -0,0 +1,30 @@
+package StackMemory;
+
+public class Testing {
+
+	public void m1() {
+		System.out.println("m1 method");
+		int i = 10;
+		m2();
+	}
+
+	public void m2() {
+		System.out.println("m2 method");
+		int i = 10;
+		m3();
+	}
+
+	public void m3() {
+		System.out.println("m3 method");
+		int i = 10;
+		
+	}
+
+	public static void main(String[] args) {
+
+		Testing tr = new Testing();
+		tr.m1();
+
+	}
+
+}
