17c5ea868e3cfb4b1f0da7de499e915ae3358287
Sathappan89 <sathappanas1989@gmail.com>
2025-11-30 00:03:19 -0700

    /Users/sathappanannamalai/Documents/javaPrograms/Dec2024Session

--- src/OOP_InterfaceConcept/TestHospital.java
+++ src/OOP_InterfaceConcept/TestHospital.java
@@ -8,116 +8,76 @@ public class TestHospital {
 		fh.entService();
 		fh.physioServices();
 		fh.orthoServices();
-		
+
 		fh.oncologyService();
 		fh.pediaService();
-		
+
 		fh.opticalServices();
 		fh.radioServices();
 		fh.cardioServices();
-		
+
 		fh.emergencyServices();
-		
+
 		fh.medicalTraining();
 		fh.oncologyService();
-		
+
 		fh.medicalRND();
 		USMedical.billing();
-		//FortisHospital.billing();
-		
-		
+		// FortisHospital.billing();
+
 		fh.medicalPolicy();
-		//Java will not allow to create interface object creation - Because interface will have only method body(i.e abstract)
-		//Example : USMedical us=new USMedical();
-		
-		/*Top Casting*/
-		/*Child class object can be referred by parent interface reference variable : Yes*/
-		/* Selenium 
-		 * WebDriver a = new ChromeDriver();
-		 * */
-		
-		
+		// Java will not allow to create interface object creation - Because interface
+		// will have only method body(i.e abstract)
+		// Example : USMedical us=new USMedical();
+
+		/* Top Casting */
+		/*
+		 * Child class object can be referred by parent interface reference variable :
+		 * Yes
+		 */
+		/*
+		 * Selenium WebDriver a = new ChromeDriver();
+		 */
+
 		USMedical.billing();
-		System.out.println(	USMedical.min_fee);
-		
-		//FortisHospital.billing(); 
-		System.out.println(	FortisHospital.min_fee);
-		
-		//USMedical.min_fee=100; //static & final in nature
-		
-		
-		
-		
+		System.out.println(USMedical.min_fee);
+
+		// FortisHospital.billing();
+		System.out.println(FortisHospital.min_fee);
+
+		// USMedical.min_fee=100; //static & final in nature
+
 		System.out.println("----Top/Up Casting----");
 		System.out.println();
 
-		
-		USMedical us=new FortisHospital();
+		USMedical us = new FortisHospital();
 		us.dentalService();
 		us.emergencyServices();
 		us.entService();
 		us.physioServices();
 		us.orthoServices();
 		us.covidVaccination();
-		//us.oncologyService(); - Reference type check will fail
-		//us.medicalTraining(); - Individual method of FortisHospital will be failed by reference type check
-		
-		
-		/*DownCasting*/
-		//We can even create parent of object creation : NOT Applicable
-		
-		UKMedical uk=new FortisHospital();
+		// us.oncologyService(); - Reference type check will fail
+		// us.medicalTraining(); - Individual method of FortisHospital will be failed by
+		// reference type check
+
+		/* DownCasting */
+		// We can even create parent of object creation : NOT Applicable
+
+		UKMedical uk = new FortisHospital();
 		uk.oncologyService();
 		uk.pediaService();
 		uk.emergencyServices();
 		uk.covidVaccination();
-		
-		//Very importance interview question
-		//interface to interface casting:
+
+		// Very importance interview question
+		// interface to interface casting:
 		USMedical usa = new FortisHospital();
-		UKMedical unm=(UKMedical)(usa);
+		UKMedical unm = (UKMedical) (usa);
 		unm.oncologyService();
 		unm.pediaService();
 		usa.dentalService();
 		usa.entService();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 
 	}
 
--- src/OOP_InterfaceConcept/USMedical.java
+++ src/OOP_InterfaceConcept/USMedical.java
@@ -1,12 +1,11 @@
 package OOP_InterfaceConcept;
 
-public interface USMedical extends WHO{
+public interface USMedical extends WHO {
 
-	//interface vars are by default static and final in nature
-	
-	int min_fee=10;
-	
-	
+	// interface vars are by default static and final in nature
+
+	int min_fee = 10;
+
 	// abstract method :
 	// no method body, only method declaration
 	// only method prototype
@@ -20,30 +19,29 @@ public interface USMedical extends WHO{
 
 	public void emergencyServices();
 
-	public int test(int i,int j);
+	public int test(int i, int j);
 
 	public void orthoServices();
-	
-	//can have private method
+
+	// can have private method : yes: but can not be overridden
 	private void budget() {
 		System.out.println("USM--budget");
 	}
-	
-	//after JDK 1.8, two major changes:
-	
-	
-	//1. static method with method body: but not abstract:
-	//can not be override
+
+	// after JDK 1.8, two major changes:
+
+	// 1. static method with method body: but not abstract:
+	// can not be override
 	public static void billing() {
 		System.out.println("USM--Billing");
 	}
-	
-	//2. default method with body: not a static:: but not abstract:
-	//can be overriden
+
+	// 2. default method with body: not a static:: but not abstract:
+	// can be overriden
 	default void medicalPolicy() {
 		System.out.println("USM-----medical policy");
 		budget();
 	}
-	
-	//Example - Testng leaner are full of default
+
+	// Example - Testng leaner are full of default
 }
