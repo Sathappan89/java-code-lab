a2a49b4b514d3852fc6beb23c4fb498d6e1228fd
Sathappan89 <sathappanas1989@gmail.com>
2025-12-02 22:59:30 -0700

    Java_25_ExceptionHandling_TryCatch

---
+++ src/ExceptionHandling/Employee.java
@@ -0,0 +1,32 @@
+package ExceptionHandling;
+public class Employee {
+	String name;
+	public static void main(String args[]) {
+		System.out.println("A");
+		System.out.println("A");
+		System.out.println("A");
+		try {
+			int p[]=new int[2];
+			p[5]=100; //AIOB
+			int i = 9 / 0; // ArithmeticException
+			Employee obj = new Employee();
+			obj = null;
+			obj.name = "naveen"; // NPE
+		} catch (ArithmeticException e) {
+			System.out.println("AE is coming...");
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			System.out.println("NPE is coming...");
+			e.printStackTrace();
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("AIOB is coming");
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			System.out.println("some exception is coming");
+			e.printStackTrace();
+		}
+		System.out.println("bye");
+	}
+}
