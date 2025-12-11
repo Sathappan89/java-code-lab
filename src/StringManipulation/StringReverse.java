015c53fb346a49474da8c9b369b658405e8c3b4c
Sathappan89 <sathappanas1989@gmail.com>
2025-12-10 22:27:25 -0700

    Java_29_StringVsStringBuilderVsStringBuffer_StringReverse_WrapperClasses_DataConversion

--- src/StringManipulation/StringReverse.java
+++ src/StringManipulation/StringReverse.java
@@ -1,15 +1,41 @@
 package StringManipulation;
 public class StringReverse {
-	public static void main(String[] args) {
-		
-		String str = "Selenium";
-		
-		StringBuilder sb = new StringBuilder("selenium");
-		System.out.println(sb.reverse());
+	public static String StringRev(String str) {
 
-		StringBuffer sbf = new StringBuffer("selenium");
-		System.out.println(sbf.reverse());
-		
+		/** null check **/
+		if (str == null) {
+			throw new RuntimeException("str is null");
+		}
+
+		/** length check **/
+		if (str.length() == 1) {
+			return str;
+		}
+
+		// length check==0
+		if (str.length() == 0) {
+			return str;
+		}
+
+		String rev = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
+		}
+
+		return rev;
 	}
 
+	public static void main(String[] args) {
+
+		String str = "Selenium Testing";
+		String res = StringReverse.StringRev(str);
+		System.out.println(res);
+//		
+//		StringBuilder sb = new StringBuilder("selenium");
+//		System.out.println(sb.reverse());
+//
+//		StringBuffer sbf = new StringBuffer("selenium");
+//		System.out.println(sbf.reverse());
+
+	}
 }
