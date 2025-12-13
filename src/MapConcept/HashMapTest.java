c418efcb30837d61ae02a70f191948f8f0f625de
Sathappan89 <sathappanas1989@gmail.com>
2025-12-12 23:07:37 -0700

    HashMap

--- src/MapConcept/HashMapTest.java
+++ src/MapConcept/HashMapTest.java
@@ -49,20 +49,18 @@ public class HashMapTest {
 		HashMap<String, String> empMap = new HashMap<String, String>();
 		System.out.println(empMap);
 		System.out.println(empMap.size());
-		
+
 		empMap.put("ravi", "SDET1");
 		empMap.put("dev", "SDET2");
 		empMap.put("jyothi", "SDET Manager");
 		empMap.put("divya", "Dev Manager");
 		empMap.put("ravi", "SDET1");
 		empMap.put("naveen", "CT0");
-		
-		
-		
+		empMap.put(null, "CEO");
+
 		System.out.println(empMap);
 		System.out.println(empMap.size());
-		//{dev=SDET2, ravi=SDET1, naveen=CT0, jyothi=SDET Manager, divya=Dev Manager}
-		
+		// {dev=SDET2, ravi=SDET1, naveen=CT0, jyothi=SDET Manager, divya=Dev Manager}
 
 		HashMap<String, Object> UserData = new HashMap<String, Object>();
 
@@ -71,11 +69,29 @@ public class HashMapTest {
 		UserData.put("designation", 14.55);
 		UserData.put("designation", true);
 
-		//Top Casting
-		Map<String,String> stMap = new HashMap<String,String>();
-		stMap = new LinkedHashMap<String,String>();
-		stMap = new TreeMap<String,String>();
-		
+		Map<String, String> sgMap = new HashMap<String, String>();
+		System.out.println(sgMap);
+		System.out.println(sgMap.size());
+
+		sgMap.put("ravi", "SDET1");
+		sgMap.put("dev", "SDET2");
+		sgMap.put("jyothi", "SDET Manager");
+		sgMap.put("divya", "Dev Manager");
+		sgMap.put("ravi", "SDET1");
+		sgMap.put("naveen", "CT0");
+		sgMap.put(null, "CEO");
+
+		System.out.println(sgMap);
+		System.out.println(sgMap.size());
+
+		// Top Casting
+		Map<String, String> stMap = new HashMap<String, String>();
+		// Hashcode calucation
+		stMap = new LinkedHashMap<String, String>();
+		// Sequence code
+		stMap = new TreeMap<String, String>();
+		// Sorted code
+
 	}
 
 }
